8f3b0a07ebeb84ac1ee1e61c36a0c9c44c04d88c
Simon Durbridge <simon.durbridge@bowerswilkins.com>
2018-10-26 16:14:59 +0100

    adapt graphic

--- journeyGraphics.js
+++ journeyGraphics.js
@@ -7,8 +7,23 @@ function myMove() {
       clearInterval(id);
     } else {
       pos++;
-      elem.style.top = pos + 'px';
       elem.style.left = pos + 'px';
     }
   }
 }
+
+// RAW
+// function myMove() {
+//   var elem = document.getElementById("animate");
+//   var pos = 0;
+//   var id = setInterval(frame, 5);
+//   function frame() {
+//     if (pos == 350) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos + 'px';
+//       elem.style.left = pos + 'px';
+//     }
+//   }
+// }
