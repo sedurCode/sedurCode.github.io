85ac8419c704221981394df4d9db27bbe412122b
Simon Durbridge <simon.durbridge@bowerswilkins.com>
2018-10-26 16:18:36 +0100

    trying to stop the thing changing height

--- journeyGraphics.js
+++ journeyGraphics.js
@@ -11,19 +11,3 @@ function myMove() {
     }
   }
 }
-
-// RAW
-// function myMove() {
-//   var elem = document.getElementById("animate");
-//   var pos = 0;
-//   var id = setInterval(frame, 5);
-//   function frame() {
-//     if (pos == 350) {
-//       clearInterval(id);
-//     } else {
-//       pos++;
-//       elem.style.top = pos + 'px';
-//       elem.style.left = pos + 'px';
-//     }
-//   }
-// }
